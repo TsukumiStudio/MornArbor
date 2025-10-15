501ef666c9873b60dd23093194b3a3e65c976fea
matsufriends <pikapikan0526@gmail.com>
2025-10-15 15:02:55 +0900

    DestroyOwnState

---
+++ Action/DestroyOwnState.cs
@@ -0,0 +1,12 @@
+﻿using Arbor;
+
+namespace MornArbor
+{
+    public class DestroyOwnState : StateBehaviour
+    {
+        public override void OnStateBegin()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
