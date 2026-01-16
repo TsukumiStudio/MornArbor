a6ce09ccfff3f923b58c2a1695a8ce13db19a477
matsufriends <pikapikan0526@gmail.com>
2026-01-16 22:28:20 +0900

    MornArborに移植

--- MornBeat/BeatStopState.cs
+++ MornBeat/BeatStopState.cs
@@ -1,4 +1,4 @@
-﻿ #if USE_ARBOR && USE_MORN_BEAT
+﻿ #if USE_MORN_BEAT
  using System.Threading;
  using Arbor;
  using UnityEngine;
---
+++ MornScene/AddAndWaitCloseSceneState.cs
@@ -0,0 +1,25 @@
+﻿using Arbor;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MornLib
+{
+    public class AddAndWaitCloseSceneState : StateBehaviour
+    {
+        [SerializeField] private MornSceneType _scene;
+        [SerializeField] private StateLink _next;
+
+        public override async void OnStateBegin()
+        {
+            await SceneManager.LoadSceneAsync(_scene.ToScene(), LoadSceneMode.Additive);
+            var scene = SceneManager.GetSceneByName(_scene.ToScene());
+            while (scene.isLoaded)
+            {
+                await UniTask.Yield(CancellationTokenOnEnd);
+            }
+
+            Transition(_next);
+        }
+    }
+}
