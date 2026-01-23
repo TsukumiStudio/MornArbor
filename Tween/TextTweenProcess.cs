6c898c63e023b0295f7430ab01baf72c78028390
matsufriends <pikapikan0526@gmail.com>
2026-01-24 02:47:20 +0900

    TextTween追加

---
+++ Tween/TextTweenProcess.cs
@@ -0,0 +1,36 @@
+using Arbor;
+using TMPro;
+using UnityEngine;
+
+namespace MornLib
+{
+    internal class TextTweenProcess : ProcessBase
+    {
+        [SerializeField] private TMP_Text _target;
+        [SerializeField] private float _duration;
+        [SerializeField] private StateLink _nextState;
+        private float _startTime;
+        private int _totalCharacters;
+        public override float Progress => Mathf.Clamp01((Time.time - _startTime) / _duration);
+
+        public override void OnStateBegin()
+        {
+            _startTime = Time.time;
+            _target.ForceMeshUpdate();
+            _totalCharacters = _target.textInfo.characterCount;
+            _target.maxVisibleCharacters = 0;
+        }
+
+        public override void OnStateUpdate()
+        {
+            var visibleCount = Mathf.FloorToInt(Progress * _totalCharacters);
+            _target.maxVisibleCharacters = visibleCount;
+
+            if (Progress >= 1f)
+            {
+                _target.maxVisibleCharacters = _totalCharacters;
+                Transition(_nextState);
+            }
+        }
+    }
+}
