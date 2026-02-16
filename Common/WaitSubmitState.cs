00c989b9fa1a8fd4746577b55fdc0015fa8713ec
matsufriends <pikapikan0526@gmail.com>
2026-02-16 13:48:26 +0900

    SelectableをWait可能に

---
+++ Common/WaitSubmitState.cs
@@ -0,0 +1,25 @@
+﻿using Arbor;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MornLib
+{
+	internal sealed class WaitSubmitState : StateBehaviour
+	{
+		[SerializeField] private Selectable _target;
+		[SerializeField] private StateLink _nextState;
+
+		public override void OnStateBegin()
+		{
+			_target.OnSubmitAsObservable().Subscribe(_ => OnClick()).AddTo(CancellationTokenOnEnd);
+		}
+
+		private void OnClick()
+		{
+			Transition(_nextState);
+		}
+	}
+}
