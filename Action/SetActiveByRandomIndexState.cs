86533b709935474612ed5ba70db51f4658737f24
matsufriends <pikapikan0526@gmail.com>
2026-03-21 03:36:32 +0900

    SetActiveByRandomIndexState追加

---
+++ Action/SetActiveByRandomIndexState.cs
@@ -0,0 +1,20 @@
+using Arbor;
+using UnityEngine;
+
+namespace MornLib
+{
+    internal class SetActiveByRandomIndexState : StateBehaviour
+    {
+        [SerializeField] private Transform _parent;
+        [SerializeField] private Vector2Int _range;
+
+        public override void OnStateBegin()
+        {
+            var activeIndex = Random.Range(_range.x, _range.y + 1);
+            for (var i = 0; i < _parent.childCount; i++)
+            {
+                _parent.GetChild(i).gameObject.SetActive(i == activeIndex);
+            }
+        }
+    }
+}
