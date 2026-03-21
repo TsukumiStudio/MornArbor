34fbc48a845759fe5b97400eced25488750c1985
matsufriends <pikapikan0526@gmail.com>
2026-03-21 17:00:39 +0900

    TransformDeltaStateにUnscaledTimeオプション追加

--- Action/TransformDeltaState.cs
+++ Action/TransformDeltaState.cs
@@ -9,10 +9,11 @@ namespace MornLib
         [SerializeField] private Vector3 _positionPerSec;
         [SerializeField] private Vector3 _rotationPerSec;
         [SerializeField] private Vector3 _scalePerSec;
+        [SerializeField] private bool _useUnscaledTime;
 
         public override void OnStateUpdate()
         {
-            var dt = Time.deltaTime;
+            var dt = _useUnscaledTime ? Time.unscaledDeltaTime : Time.deltaTime;
             _target.localPosition += _positionPerSec * dt;
             _target.localEulerAngles += _rotationPerSec * dt;
             _target.localScale += _scalePerSec * dt;
